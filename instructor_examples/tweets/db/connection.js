e59847912946105cfef7d5622c5fe0921147dc6b
Joe Keohan <jkeohan@gmail.com>
2020-10-15 12:08:11 -0400

    mongoose connection setup

---
+++ instructor_examples/tweets/db/connection.js
@@ -0,0 +1,22 @@
+// IMPORT MONGOOSE
+const mongoose = require('mongoose')
+// CONNECTION URI
+const mongoURI = 'mongodb://localhost:27017/tweets'
+// CONFIG PARAMS
+const config = {
+    useUnifiedTopology: true, 
+    useNewUrlParser: true 
+}
+// CONNECT TO THE DATABASE/URI
+mongoose.connect(mongoURI, config)
+// CREATE A CONNECTION 
+const db = mongoose.connection
+// ADDITIONAL MESSAGE CONNECTIONS
+// ADDITIONAL MESSAGE CONNECTIONS
+db.on('error', (err) => console.log(err.message + ' is mongod not running?'));
+db.on('connected', () => console.log('mongo connected: ', mongoURI));
+db.on('disconnected', () => console.log('mongo disconnected'));
+// CLOSE THE CONNECTION
+db.close()
+// EXPORT MONGOOSE
+module.exports = mongoose

2c3870778a2b979fc1c013a5259911b39a871f8c
Joe Keohan <jkeohan@gmail.com>
2020-10-15 13:26:22 -0400

    npm run db:refactor seed data to use .then() and added startup script

--- instructor_examples/tweets/db/seed.js
+++ instructor_examples/tweets/db/seed.js
@@ -3,19 +3,27 @@ const mongoose = require('./connection')
 // IMPORT THE TWEET MODEL
 const Tweet = require('../models/tweet')
 // IMPORT THE TWEETS JSON DATA
-const tweetsData = require('./seedData.json')
+const manyTweets= require('./seedData.json')
 // CREATE THE DB CONNECTION
 const db = mongoose.connection
-// CREATE ALL THE TWEETS
-Tweet.insertMany(tweetsData, (error, tweets) => {
-	if (error) {
-		console.log('error', error);
-	} else {
+// CREATE ALL TWEETS USING .THEN()
+Tweet.deleteMany({}).then(() => {
+	Tweet.insertMany(manyTweets).then((tweets) => {
 		console.log('tweets', tweets);
-	}
-	// CLOSE THE CONNECTION TO THE DB
-	db.close();
+		db.close();
+	});
 });
+
+// CREATE ALL THE TWEETS USING A CALLBACK 
+// Tweet.insertMany(tweetsData, (error, tweets) => {
+// 	if (error) {
+// 		console.log('error', error);
+// 	} else {
+// 		console.log('tweets', tweets);
+// 	}
+// 	// CLOSE THE CONNECTION TO THE DB
+// 	db.close();
+// });
 
 // OUR FIRST TWEET
 // const myFirstTweet = {

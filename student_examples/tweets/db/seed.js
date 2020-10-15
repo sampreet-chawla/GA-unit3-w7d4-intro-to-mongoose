fbd0542a4bc6b87ef0e238ef6df5cab12d2b7092
Sampreet <sam.learnnew@gmail.com>
2020-10-15 10:26:22 -0700

    Using seedData.json in seed.js using Promises, also added script to run seed.js

--- student_examples/tweets/db/seed.js
+++ student_examples/tweets/db/seed.js
@@ -3,17 +3,27 @@ const Tweet = require('../models/tweet');
 const db = mongoose.connection;
 const manyTweets = require('./seedData.json');
 
-// Feed in manyTweets
-Tweet.insertMany(manyTweets, (error, tweets) => {
-	if (error) {
-		console.log('error', error);
-	} else {
+// CREATE ALL TWEETS USING .THEN()
+// Delete all earlier data to avoid duplicates.
+Tweet.deleteMany({}).then(() => {
+	console.log('Earlier tweets deleted');
+	Tweet.insertMany(manyTweets).then((tweets) => {
 		console.log('tweets', tweets);
-	}
-	db.close();
+		db.close();
+	});
 });
 
-// Out First Tweet data
+// FEED IN MANY TWEETS USING CALLBACK
+// Tweet.insertMany(manyTweets, (error, tweets) => {
+// 	if (error) {
+// 		console.log('error', error);
+// 	} else {
+// 		console.log('tweets', tweets);
+// 	}
+// 	db.close();
+// });
+
+// OUR FIRST TWEET DATA
 // const myFirstTweet = {
 // 	title: 'Deep Thoughts',
 // 	body: 'Friends, I have been navel-gazing',
